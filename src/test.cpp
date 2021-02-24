8515ce3c03d4b914cf24415d57cc223470fd5111
KevinChiu <fsd83811@gmail.com>
2021-02-24 15:12:11 +0800

    vmu931_ex

---
+++ src/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>     
+using namespace std;
+
+// #include "ros/ros.h"
+// #include "std_msgs/String.h"
+
+// #include <sstream>
+
+int main(int argc, char **argv) {
+
+    // ros::init(argc, argv, "talker");
+    // ros::NodeHandle n;
+
+    while (1)
+    {
+        cout << "Hello! World!\n"; 
+    }
+
+    return 0; 
+}
